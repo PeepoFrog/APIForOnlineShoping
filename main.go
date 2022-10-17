8193aeffd1fc8d0c1f9ced5ae7dceec4b0ebfee3
DmitriyBrch <organizm999@gmail.com>
2022-10-17 17:44:31 +0300

    Added gracefullshutdown

--- main.go
+++ main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"flag"
+	"internetshop/helper"
 	"internetshop/router"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
+	var wait time.Duration
+	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.Parse()
 	r := router.Router()
-	fmt.Println("****SERVER-----MONGO----DB-----STARTing****")
-	log.Fatal(http.ListenAndServe(":4000", r))
-	fmt.Println("****LISTENING AP PORT 4000****")
+	srv := &http.Server{
+		Addr: "0.0.0.0:4000",
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		// WriteTimeout: time.Second * 15,
+		// ReadTimeout:  time.Second * 15,
+		// IdleTimeout:  time.Second * 60,
+		Handler: r, // Pass our instance of gorilla/mux in.
+	}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	srv.Shutdown(ctx)
+	postgres := helper.NewPostgre()
+	postgres.StopPostgreConnection()
+
+	log.Println("shutting down")
+	os.Exit(0)
 
 }
 
